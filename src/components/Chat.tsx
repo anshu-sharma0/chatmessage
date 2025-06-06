5ad0f0d3b11e0f5f3b9daeafcaffa362f57f9f3b
Anshu <ashu.dnn@gmail.com>
2025-06-06 16:02:40 +0530

    fix type

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,5 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import type {SubmitHandler} from "react-hook-form";
 
 type Inputs = {
     message: string,
